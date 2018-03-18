7e4f5a02ae33e993ba46f7683088753c39cd4028
r0hitsharma <rs@rohit.sh>
2018-03-18 16:31:54 +0100

    Adding state in TodoList class and form for adding new todo.

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,29 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class TodoList extends React.Component {
+class TodoList extends Component {
 	constructor(props) {
-		console.log("TodoList constructor");
+		// console.log("TodoList constructor");
 		super(props);
+		this.addTodo = this.addTodo.bind(this);
+
+		this.state = {
+			todos: this.props.todos
+		};
+
+		this.counter = Math.max(...this.state.todos.map(todo => todo.id));
 	}
 
 	render() {
 		return (
 			<div>
+				<form onSubmit={ this.addTodo }>
+					<label>Add new todo: </label>
+					<input
+						placeholder="todo item"
+						ref={(input) => { this.newTodoInput = input }}
+						required
+					/>
+					<input type="submit" value="Add"/>
+				</form>
 				<ul>
 				{
-					this.props.todos.map(todo => <li>{ todo.name }</li>)
+					this.state.todos.map(todo => <li key={ todo.id }>{ todo.name }</li>)
 				}
 				</ul>
 			</div>
 		);
 	}
 
+	addTodo(e) {
+		// stop the form from navigating away
+		e.preventDefault();
+
+		// console.log(this.props.todos);
+		const newTodo = {
+			id: ++this.counter,
+			name: this.newTodoInput.value
+		};
+		console.log("adding Todo:", newTodo);
+		this.setState({ todos: [...this.state.todos, newTodo]});
+	}
+
 	componentWillMount() {
-		console.log("TodoList component mounting");
+		// console.log("TodoList component mounting");
 	}
 
 	componentDidMount() {
-		console.log("TodoList component mounted");
+		// console.log("TodoList component mounted");
 	}
 };
 
